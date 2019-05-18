a807e9547e6ad9fe7b49b7800e9e68d3afa5652e
Jason Sanchez <jason@xao.live>
2019-05-17 22:07:11 -0600

    Storyboard configuration

---
+++ RxCamera/RxCamera/CameraFilterController.swift
@@ -0,0 +1,30 @@
+//
+//  CameraFilterController.swift
+//  RxCamera
+//
+//  Created by Jason Sanchez on 5/17/19.
+//  Copyright © 2019 Jason Sanchez. All rights reserved.
+//
+
+import UIKit
+
+class CameraFilterController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
